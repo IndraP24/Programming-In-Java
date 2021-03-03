f3ab69cc681d173ef7acd4b2ebdc64844f98b269
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2021-03-03 20:15:06 +0530

    Updated comments

--- src/Language/VarString.java
+++ src/Language/VarString.java
@@ -1,11 +1,13 @@
 package Language;
 
-// The proper way to declare a class(String) variable
+// The proper way to declare a class(String) variable or a local variable
 
 public class VarString {
     static String helloMessage;
 
     public static void main (String[] args) {
+        // String helloMessage;
+        // Declared as a local variable
         helloMessage = "Hello, World!";
         System.out.println(helloMessage);
     }
