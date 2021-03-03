52e6f1d74619c3f30ada5fc7c2736839b7066d68
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2021-03-03 20:12:00 +0530

    Added VarString

---
+++ src/Language/VarString.java
@@ -0,0 +1,12 @@
+package Language;
+
+// The proper way to declare a class(String) variable
+
+public class VarString {
+    static String helloMessage;
+
+    public static void main (String[] args) {
+        helloMessage = "Hello, World!";
+        System.out.println(helloMessage);
+    }
+}
